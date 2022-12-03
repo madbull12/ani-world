8dcecf30ff9abde6ed10247ab8ac4b6afa4c2189
madbull12 <huangandrian02@gmail.com>
2022-12-03 23:39:32 +0700

    another huge updates

--- pages/api/favorite/[slug].ts
+++ pages/api/favorite/[slug].ts
@@ -8,22 +8,7 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse) {
     const session:any = await getSession();
     const animeId = req.query.slug;
     console.log(session);
-    if(req.method==="GET") {
-        try {
-            const data = await prisma.favouriteAnime.findMany({
-                where:{
-                    userId:session?.user.id as string
-                }
-            });
 
-            res.status(200).json(data);
-         
-        } catch(err) {
-            res.status(400).json({err:err})
-        }
- 
-        
-    } 
      if(req.method==="DELETE"){
         const anime = await prisma.favouriteAnime.delete({
             where:{
