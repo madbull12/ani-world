844df979800e9015bde0bb5ed17eec3993e8d963
madbull12 <huangandrian02@gmail.com>
2023-05-20 01:09:01 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,7 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+  experimental: { appDir: false },
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
    * must comment the below `i18n` config out.
